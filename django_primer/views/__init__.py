c5db64d8b1055ce483d8f1f4f1710049aed8f33b
Vladislav Sidorov <sidlaf@ya.ru>
2024-06-05 15:44:47 +0700

    frontend

--- django_primer/views/__init__.py
+++ django_primer/views/__init__.py
@@ -1,5 +1,5 @@
 from .index_view import IndexView
 from .report_views import ReportView
 from .score_views import ScoreListView, ScoreCreateView, ScoreUpdateView, ScoreDeleteView
-from .student_views import StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView, StudentPerformanceSummaryView
+from .student_views import StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView
 from .subject_views import SubjectListView, SubjectCreateView, SubjectDeleteView, SubjectUpdateView
--- django_primer/views/student_views.py
+++ django_primer/views/student_views.py
@@ -1,4 +1,5 @@
 from django.urls import reverse_lazy
+from django import forms
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView,TemplateView
 from ..models import Student, Subject, Score
 
@@ -16,6 +17,9 @@ class StudentUpdateView(UpdateView):
     model = Student
     template_name = 'student_form.html'
     fields = ['name', 'birth_date']
+    widgets = {
+    'birth_date': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
+}
     success_url = reverse_lazy('student_list')
 
 class StudentDeleteView(DeleteView):
@@ -23,27 +27,3 @@ class StudentDeleteView(DeleteView):
     template_name = 'student_confirm_delete.html'
     success_url = reverse_lazy('student_list')
 
-
-class StudentPerformanceSummaryView(TemplateView):
-    template_name = 'student_performance_summary.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        subjects = Subject.objects.all()
-        students = Student.objects.all()
-
-        student_statistics = []
-
-        for student in students:
-            scores = []
-            for subject in subjects:
-                score = Score.objects.filter(student=student, subject=subject).first()
-                scores.append(score.value if score else '-')
-            student_statistics.append({
-                'student': student,
-                'scores': scores
-            })
-
-        context['subjects'] = subjects
-        context['student_statistics'] = student_statistics
-        return context
